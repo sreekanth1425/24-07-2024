9fefe38638f41ed9891505048df08533bae74e98
sreekanth1425 <135114730+sreekanth1425@users.noreply.github.com>
2024-07-24 15:57:51 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -3,7 +3,7 @@ public class Jenkins {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i + ".Love You NANNA");
-System.out.println(i + ".You Have Encouraged Me Like That I Can Survive Anywhere And In Any Situation");
+                         System.out.println(i + ".You Have Encouraged Me Like That I Can Survive Anywhere And In Any Situation");
 
 		}
 	}
